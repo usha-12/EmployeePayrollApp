cbd7a67cbec0e1a4256b840756e52f75accbd38c
usha-12 <usha12ahirwar@gmail.com>
2022-12-06 14:24:10 +0530

    Display employee details from json object

--- JS/home.js
+++ JS/home.js
@@ -4,24 +4,58 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
 const createInnerHtml = () => {
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
-    const innerHtml = `${headerHtml}
+    let innerHtml = `${headerHtml}`
+    let employeePayrollList = createEmployeePayrollJSON();
+    for (const employee of employeePayrollList) {
+        innerHtml = `${innerHtml}
     <tr>
     <td>
-        <img class="profile" alt="" src="../assets/profile-images/Ellipse -3.png">
+        <img class="profile" alt="" src="${employee._picture}">
     </td>
-    <td>Anirudh Singh</td>
-    <td>Male</td>
+    <td>${employee._name}</td>
+    <td>${employee._gender}</td>
+    <td>${getDeptHtml(employee._department)}</td>
+    <td>${employee._salary}</td>
+    <td>${employee._startDate}</td>
     <td>
-        <div class="dept-label">HR</div>
-        <div class="dept-label">Engineering</div>
-    </td>
-    <td>600000</td>
-    <td>30 sep 2016</td>
-    <td>
-        <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-        <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+        <img id="${employee._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+        <img id="${employee._id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
     </td>
  </tr>
     `;
+    }
     document.querySelector("#display").innerHTML = innerHtml;
+};
+
+const createEmployeePayrollJSON = () => {
+    let employeeListLocal = [{
+            _name: "Alex",
+            _gender: "Male",
+            _department: ["Finance", "Sales"],
+            _salary: "700000",
+            _startDate: "26 jan 2018",
+            _note: "",
+            _id: new Date().getTime(),
+            _picture: "../assets/profile-images/Ellipse -5.png"
+        },
+        {
+            _name: "Liya",
+            _gender: "Female",
+            _department: ["Finance", "Engineering"],
+            _salary: "750000",
+            _startDate: "17 Dec 2018",
+            _note: "",
+            _id: new Date().getTime() + 1,
+            _picture: "../assets/profile-images/Ellipse -7.png"
+        }
+    ];
+    return employeeListLocal;
+};
+
+const getDeptHtml = (departmentList) => {
+    let deptHtml = "";
+    for (const dept of departmentList) {
+        deptHtml = `${deptHtml}<div class="dept-label">${dept}</div>`
+    }
+    return deptHtml;
 };
--- JS/utility.js
+++ JS/utility.js
@@ -1,7 +1,5 @@
 const stringifyDate = (date) => {
     const options = { day: 'numeric', month: 'long', year: 'numeric' };
     const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString("en-US", options);
-    
     return newDate;
-
 }
